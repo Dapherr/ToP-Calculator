c96b514c1b4c761355ce6aab97fb8c04e1b9f146
David <derocherdavid@gmail.com>
2026-02-01 12:04:01 -0500

    Add calculator body and initial calculation function

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
+const digitButtons = document.querySelectorAll('.digit');
+const operatorButtons = document.querySelectorAll('.operator');
+const clearButton = document.querySelector('.clear');
+const displayOutput = document.querySelector('#display');
+
 let num1 = 0;
 let num2 = 0;
 let operator = '';
@@ -22,3 +27,19 @@ function operate(operator, num1, num2) {
         result = 'Please enter a valid operator';
     }
     return result;
+};
+
+digitButtons.forEach(digitButton => {
+    digitButton.addEventListener("click", () => {
+        // return digitButton.textContent;
+        console.log(digitButton.textContent);
+    })
+})
+
+operatorButtons.forEach(operatorButton => {
+    operatorButton.addEventListener("click", () => {
+        // return operatorButton.textContent;
+        console.log(operatorButton.textContent);
+    })
+})
+
